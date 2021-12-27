5bb2156c28e0e6f1210ad62cfeb39ae327a5a0fc
Wiatagan Paz <pazwiatagan@gmail.com>
2021-12-27 16:39:41 -0300

    IP da api foi alterado

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:3333/v1/',
+  baseURL: 'http://192.168.0.105:3333/v1/',
+  // baseURL: 'http://127.0.0.1:3333/v1/',
   headers: {'Access-Control-Allow-Origin': '*'}
 })
 
