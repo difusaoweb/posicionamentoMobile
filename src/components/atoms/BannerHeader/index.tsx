c68338cf946d0a23e812f8f0fd45cbea1a13411e
Wiatagan Paz <pazwiatagan@gmail.com>
2021-12-27 16:45:12 -0300

    Mudança na estrutura dos components

--- src/components/atoms/BannerHeader/index.tsx
+++ src/components/atoms/BannerHeader/index.tsx
@@ -1,45 +1,51 @@
-import React, { useState } from 'react'
-import {
-  AdMobBanner,
-  setTestDeviceIDAsync,
-} from 'expo-ads-admob'
-import Constants from 'expo-constants'
+import React from 'react'
+import { AdMobBanner } from 'expo-ads-admob'
 import * as Device from 'expo-device'
-import { View } from 'react-native'
+import { View, StyleSheet } from 'react-native'
+import { useTheme } from 'react-native-paper'
 
-export function BannerHeader() {
-  // async () => {
-  //   await setTestDeviceIDAsync('EMULATOR')
-  // }
-
+const BannerHeader = () => {
   const devAdUnitID = 'ca-app-pub-3940256099942544/6300978111'
   const androidAdUnitID = 'ca-app-pub-8947654147770289/2160324592'
   const iosAdUnitID = 'ca-app-pub-8947654147770289/2876695804'
-
-  // if(Device.osName) {
-  //   if(Device.osName === 'Android') {
-  //     setMobilePlatform('android')
-  //   }
-  //   else if(Device.osName == 'iOS') {
-  //     setMobilePlatform('iOS')
-  //   }
-  // }
-
   const adUnitID = Device.isDevice && !__DEV__ ? (Device.osName === 'Android' ? androidAdUnitID : iosAdUnitID) : devAdUnitID
 
+  const { colors } = useTheme()
+
   return (
-    <>
+    <View style={[styles.container, { backgroundColor: colors.background }]}>
       {
         Device.brand ?
         <AdMobBanner
-          bannerSize="fullBanner"
+          bannerSize="banner"
           adUnitID={adUnitID}
           servePersonalizedAds={true}
           onDidFailToReceiveAdWithError={(error)=> { console.log(error) }}
+          style={styles.banner}
         />
         :
-        <View style={{ backgroundColor: '#f00', height: 40, width: '100%' }}></View>
+        null
       }
-    </>
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 54,
+    maxHeight: 54,
+    width: '100%',
+    maxWidth: '100%'
+  },
+  banner: {
+    justifyContent: 'center',
+    alignSelf: 'center',
+    maxHeight: 54,
+    maxWidth: '100%'
+  }
+})
+
+export default BannerHeader
