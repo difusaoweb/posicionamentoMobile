c0872549aba7dbbf48f540b4b9d1f34f949cc640
Wiatagan Paz <pazwiatagan@gmail.com>
2022-01-02 11:14:52 -0300

    Rotas reformadas

--- src/routes/App/index.routes.tsx
+++ src/routes/App/index.routes.tsx
@@ -1,86 +1,20 @@
-import * as React from 'react'
-import { FontAwesome5 } from '@expo/vector-icons'
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
-import { useTheme } from 'react-native-paper'
-import AppLoading from 'expo-app-loading'
+import React from 'react'
+import  { createStackNavigator } from '@react-navigation/stack'
 
-import { useAppSelector, useAppDispatch } from '../../hooks'
-import { loadStorageDataAsync, isSigned as isSignedRedux } from '../../redux/reducers/access'
-import HomePage from '../../pages/Home'
-import SearchPage from '../../pages/Search'
-import AddPage from '../../pages/Add'
-import ProfilePage from '../../pages/Profile'
+import TabRoutes from './tab.routes'
+import AffirmationPage from '../../pages/Affirmation'
 
-const Tab = createMaterialBottomTabNavigator()
+const AppStack = createStackNavigator()
 
-const AppRoutes: React.FC = () => {
-  const [isReady, setIsReady] = React.useState(true)
-  const signed = useAppSelector(isSignedRedux)
-  const dispatch = useAppDispatch()
-
-  const { colors } = useTheme()
-
-  React.useEffect(() => {
-    function loadStorageData() {
-      dispatch(loadStorageDataAsync())
-      setIsReady(false)
-    }
-    loadStorageData()
-  }, [])
-
-  if(isReady)
-    return <AppLoading />
-
-  return (
-    <Tab.Navigator
-      initialRouteName="SearchPage"
-      // initialRouteName="HomePage"
-      activeColor={colors.text}
-      inactiveColor={colors.disabled}
-      barStyle={{ backgroundColor: colors.primary }}
-    >
-      <Tab.Screen
-        name="HomePage"
-        component={HomePage}
-        options={{
-          tabBarLabel: 'Ínicio',
-          tabBarIcon: ({ color }) => (
-            <FontAwesome5 name="home" color={color} size={20} />
-          )
-        }}
-      />
-      <Tab.Screen
-        name="SearchPage"
-        component={SearchPage}
-        options={{
-          tabBarLabel: 'Procurar',
-          tabBarIcon: ({ color }) => (
-            <FontAwesome5 name="search" color={color} size={20} />
-          )
-        }}
-      />
-      <Tab.Screen
-        name="AddPage"
-        component={AddPage}
-        options={{
-          tabBarLabel: 'Adicionar',
-          tabBarIcon: ({ color }) => (
-            <FontAwesome5 name="plus" color={color} size={20} />
-          )
-        }}
-      />
-      <Tab.Screen
-        name="ProfilePage"
-        component={ProfilePage}
-        options={{
-          tabBarLabel: 'Perfil',
-          tabBarIcon: ({ color }) => (
-            <FontAwesome5 name="user-alt" color={color} size={20} />
-          )
-        }}
-      />
-    </Tab.Navigator>
-  )
-}
+const AppRoutes: React.FC = () => (
+  <AppStack.Navigator headerMode="none" >
+    <AppStack.Screen
+      name="TabRoutes"
+      component={TabRoutes}
+      options={{ headerShown: false }}
+    />
+    <AppStack.Screen name="AffirmationPage" component={AffirmationPage} />
+  </AppStack.Navigator>
+)
 
 export default AppRoutes
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,11 +10,7 @@ const Stack = createStackNavigator()
 const Routes = () => {
   return(
     <Stack.Navigator headerMode="none">
-      <Stack.Screen
-        name="AppRoutes"
-        component={AppRoutes}
-        options={{ headerShown: false }}
-      />
+      <Stack.Screen name="AppRoutes" component={AppRoutes} />
       <Stack.Screen name="AuthRoutes" component={AuthRoutes} />
     </Stack.Navigator>
   )
