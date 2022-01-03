0821dc382d57ff0321afad1043db7c1138eb097a
Wiatagan Paz <pazwiatagan@gmail.com>
2022-01-03 11:56:48 -0300

    Redux AffirmationPage

---
+++ src/redux/reducers/affirmationPage.ts
@@ -0,0 +1,9 @@
+export interface singleAffirmationInterface {
+  id: number
+  message: string
+  stronglyAgree: number
+  agree: number
+  neutral: number
+  disagree: number
+  stronglyDisagree: number
+}
